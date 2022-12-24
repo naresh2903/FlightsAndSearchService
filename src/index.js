be2c63cf0308b2aa83837eeec223f1e87b243e13
narendra <np9634464272@gmail.com>
2022-12-24 23:38:20 +0530

    added unique and started cityrepo

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ const express = require('express');
 const bodyParser = require('body-parser')
 
 const {PORT} = require('./config/serverConfig')
+
+// testing repo
+const CityRepository = require("./repository/cityRepo")
+
 const SetupAndStartServer = async()=>{
 
     //create express object
@@ -15,7 +19,11 @@ const SetupAndStartServer = async()=>{
 
 
     app.listen(PORT , ()=>{
-        console.log(`server is running fine on ${PORT}`)
+    console.log(`server is running fine on ${PORT}`)
+      
+    // demo for using repositories
+    const repo = new  CityRepository();
+      repo.createCity({name:"New Delhi"});
     })
 }
 
