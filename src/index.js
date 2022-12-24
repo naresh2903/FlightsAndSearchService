884c01b059b4da6f46136308773e62dd549ff1d5
narendra <np9634464272@gmail.com>
2022-12-24 21:40:26 +0530

    complete sequelize and db create

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 const express = require('express');
-const app = express();
+
+const bodyParser = require('body-parser')
 
 const {PORT} = require('./config/serverConfig')
 const SetupAndStartServer = async()=>{
+
+    //create express object
+    const app = express();
+
+
+    // middlewares
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended:true}));
+
+
     app.listen(PORT , ()=>{
         console.log(`server is running fine on ${PORT}`)
     })
 }
 
 
-app.get("/",(req , res)=>{
-   
-    res.status(200).json({"message" : `working fine on ${PORT}`});
-})
+
 
 SetupAndStartServer();
